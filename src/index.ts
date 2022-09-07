0dbe27d0d7ddc7dd8ce5fe83ccae6af7097644b8
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2022-09-07 15:53:05 -0400

    init commit

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import express from "express";
+import bodyParser from "body-parser";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import router from "./api";
+import cors from "cors";
+
+const app = express()
+const port = 4000
+
+const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "eShop - Catalog HTTP API",
+        version: "0.1.0",
+        description:
+          "The Catalog service HTTP API. THis is a Data-Driven/CRUD sample.",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:3000/",
+        },
+      ],
+    },
+    apis: ["./api.js"],
+  };
+  
+const specs = swaggerJsdoc(options);
+
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
+
+app.use(cors());
+
+app.use(router);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
