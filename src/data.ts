05b03e1c0b202f9ef43da6743bfdfae02f9f2635
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2022-09-07 19:02:07 -0400

    added images to /api path references

--- src/data.ts
+++ src/data.ts
@@ -6,49 +6,49 @@ const items = [
         id: "1",
         name: ".NET Black & White Mug",
         price: 8.5,
-        pictureUri: "/images/2.png"
+        pictureUri: "/api/images/2.png"
     },
     {
         id: "2",
         name: ".NET Blue Hoodie",
         price: 12,
-        pictureUri: "/images/6.png"
+        pictureUri: "/api/images/6.png"
     },
     {
         id: "3",
         name: ".NET Bot Black Hoodie",
         price: 19.50,
-        pictureUri: "/images/1.png"
+        pictureUri: "/api/images/1.png"
     },
     {
         id: "4",
         name: ".NET Foundation Pin",
         price: 12,
-        pictureUri: "/images/10.png"
+        pictureUri: "/api/images/10.png"
     },
     {
         id: "5",
         name: ".NET Purple Shirt",
         price: 18.5,
-        pictureUri: "/images/4.png"
+        pictureUri: "/api/images/4.png"
     },
     {
         id: "6",
         name: ".NET Cup Pin",
         price: 7.5,
-        pictureUri: "/images/11.png"
+        pictureUri: "/api/images/11.png"
     },
     {
         id: "7",
         name: ".NET Cup Mug",
         price: 13.50,
-        pictureUri: "/images/9.png"
+        pictureUri: "/api/images/9.png"
     },
     {
         id: "8",
         name: ".NET Foundation Purple Hoodie",
         price: 22,
-        pictureUri: "/images/8.png"
+        pictureUri: "/api/images/8.png"
     },
 ]
 
