09f5d2c0cea99fe63b9f74c06f52f0b360947671
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2022-09-08 18:18:14 -0400

    changed images to remove branding

--- src/data.ts
+++ src/data.ts
@@ -4,51 +4,51 @@
 const items = [
     {
         id: "1",
-        name: ".NET Black & White Mug",
+        name: "Blue Shirt",
         price: 8.5,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/2.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/blue_shirt.png"
     },
     {
         id: "2",
-        name: ".NET Blue Hoodie",
+        name: "Orange Shirt Shirt",
         price: 12,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/6.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/orange_shirt.png"
     },
     {
         id: "3",
-        name: ".NET Bot Black Hoodie",
+        name: "Violet Shirt",
         price: 19.50,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/1.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/violet_shirt.png"
     },
     {
         id: "4",
-        name: ".NET Foundation Pin",
+        name: "Green Shirt",
         price: 12,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/10.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/green_shirt.png"
     },
     {
         id: "5",
-        name: ".NET Purple Shirt",
+        name: "Purple Shirt",
         price: 18.5,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/4.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/purple_shirt.png"
     },
     {
         id: "6",
-        name: ".NET Cup Pin",
+        name: "Black Shirt",
         price: 7.5,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/11.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/black_shirt.png"
     },
     {
         id: "7",
-        name: ".NET Cup Mug",
+        name: "Yellow Shirt",
         price: 13.50,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/9.png"
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/yellow_shirt.png"
     },
     {
         id: "8",
-        name: ".NET Foundation Purple Hoodie",
-        price: 22,
-        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/8.png"
+        name: "Pink Shirt",
+        price: 8.5,
+        pictureUri: "https://igniteeshopswa.blob.core.windows.net/images/pink_shirt.png"
     },
 ]
 
